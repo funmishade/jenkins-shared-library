29a2bb34616cde9d64a187b2567f71941722e0de
Funmi Adewale <funmi.adewalemichael@gmail.com>
2026-04-05 06:09:54 -0300

    using parameter

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,22 @@
 
-def call () {
+// def call () {
+//     echo "Building the Docker image..."
+//     withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+//         sh "docker build -t funmishade/demo-app:jma-2.0 ."
+//         sh "echo $PASS | docker login -u $USER --password-stdin"
+//         sh "docker push funmishade/demo-app:jma-2.0"
+//     }
+
+// }
+
+
+
+def call (string imageName) {
     echo "Building the Docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh "docker build -t funmishade/demo-app:jma-2.0 ."
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push funmishade/demo-app:jma-2.0"
+        sh "docker push $imageName"
     }
 
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,12 @@
 #!/user/bin/env groovy 
+// def call() {
+//     echo 'Building the application...'
+//     sh 'mvn package'
+
+// }
+
 def call() {
-    echo 'Building the application...'
+    echo 'Building the application for branch $GIT_BRANCH...'
     sh 'mvn package'
 
 }
